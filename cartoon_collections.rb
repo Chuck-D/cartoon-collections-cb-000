af328eaf57d5059d773dda5b21eeaf17ef9569f3
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-19 20:16:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ end
 
 def find_the_cheese(strings)
     cheese_types = ["cheddar", "gouda", "camembert"]
-    if strings.detect {|string| string==cheese_types.any}
+    strings.detect {|string| string==cheese_types.any}
     end
 end
