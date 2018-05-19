6a38453e805d96ddc85b917c1c9bfe2604a2a912
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-19 20:15:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ end
 
 def find_the_cheese(strings)
     cheese_types = ["cheddar", "gouda", "camembert"]
-    if strings.include?("cheddar") {|string| return string}
+    if strings.detect {|string| string}
     end
 end
